a44950329ef95ad806e51ecf0663de55b4c64f82
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-03-28 21:42:36 +0300

    add more structure start adding model

---
+++ wallchat.Api/App_Start/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(wallchat.Api.App_Start.Startup))]
+
+namespace wallchat.Api.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ wallchat.Model/App/Entity/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace wallchat.Model.App.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        [ StringLength ( 14 ) ]
+        public string UserName { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        [ DataType ( DataType.EmailAddress ) ]
+        public string Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool PhoneConfirmed { get; set; }
+
+        [ DataType ( DataType.DateTime ) ]
+        public DateTime DateRegistration { get; set; }
+
+        public int AccessFailedCount { get; set; }
+    }
+}
