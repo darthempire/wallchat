4cfa68379c637cd6d37f188b8cff38d3be902575
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-03-29 23:47:33 +0300

    add SHA256 crypto algorithm

---
+++ wallchat.CustomProvider/App/Core/SimpleRefreshTokenProvider.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Infrastructure;
+using wallchat.CustomProvider.App.Repository;
+using wallchat.Model.App.Entity;
+
+namespace wallchat.CustomProvider.App.Core
+{
+    public class SimpleRefreshTokenProvider : IAuthenticationTokenProvider
+    {
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            var clientid = context.Ticket.Properties.Dictionary["as:client_id"];
+
+            if (string.IsNullOrEmpty(clientid))
+                return;
+
+            var refreshTokenId = Guid.NewGuid().ToString("n");
+
+            using (var _repo = new AuthRepository())
+            {
+                var refreshTokenLifeTime = context.OwinContext.Get<string>("as:clientRefreshTokenLifeTime");
+
+                var token = new RefreshToken
+                {
+                    Id = Crypto.GetHash(refreshTokenId),
+                    ClientId = clientid,
+                    Subject = context.Ticket.Identity.Name,
+                    IssuedUtc = DateTime.UtcNow,
+                    ExpiresUtc = DateTime.UtcNow.AddMinutes(Convert.ToDouble(refreshTokenLifeTime))
+                };
+
+                context.Ticket.Properties.IssuedUtc = token.IssuedUtc;
+                context.Ticket.Properties.ExpiresUtc = token.ExpiresUtc;
+
+                token.ProtectedTicket = context.SerializeTicket();
+
+                var result = _repo.AddRefreshToken(token);
+
+                if (result)
+                    context.SetToken(refreshTokenId);
+            }
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            var allowedOrigin = context.OwinContext.Get<string>("as:clientAllowedOrigin");
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+
+            var hashedTokenId = Crypto.GetHash(context.Token);
+
+            using (var _repo = new AuthRepository())
+            {
+                var refreshToken = _repo.FindRefreshToken(hashedTokenId);
+
+                if (refreshToken != null)
+                {
+                    //Get protectedTicket from refreshToken class
+                    context.DeserializeTicket(refreshToken.ProtectedTicket);
+                    var result = _repo.RemoveRefreshToken(hashedTokenId);
+                }
+            }
+        }
+
+        public async Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+            var allowedOrigin = context.OwinContext.Get<string>("as:clientAllowedOrigin");
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+
+            var hashedTokenId = Crypto.GetHash(context.Token);
+
+            using (var _repo = new AuthRepository())
+            {
+                var refreshToken = _repo.FindRefreshToken(hashedTokenId);
+
+                if (refreshToken != null)
+                {
+                    //Get protectedTicket from refreshToken class
+                    context.DeserializeTicket(refreshToken.ProtectedTicket);
+                    var result = _repo.RemoveRefreshToken(hashedTokenId);
+                }
+            }
+        }
+    }
+}
---
+++ wallchat.Helpers/Cryptography/SHA256.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace wallchat.Helpers.Cryptography
+{
+    public static class SHA256
+    {
+        public static string GetHash ( string input )
+        {
+            HashAlgorithm hashAlgorithm = new SHA256CryptoServiceProvider( );
+
+            var byteValue = Encoding.UTF8.GetBytes (input);
+
+            var byteHash = hashAlgorithm.ComputeHash (byteValue);
+
+            return Convert.ToBase64String (byteHash);
+        }
+    }
+}
