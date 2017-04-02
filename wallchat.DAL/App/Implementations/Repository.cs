d530e4d6a865dc0272d4e23297ece294935cae86
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-04-02 23:39:17 +0300

    add error loggin in repository

--- wallchat.DAL/App/Implementations/Repository.cs
+++ wallchat.DAL/App/Implementations/Repository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
+using NLog;
 using wallchat.DAL.App.Contracts;
 using wallchat.Helpers.Exceptions;
 using wallchat.Model.App.EF;
@@ -13,16 +14,18 @@ namespace wallchat.DAL.App.Implementations
     {
         private readonly IDbSet<T> _dbset;
         private DatabaseContext _dataContext;
+        private readonly Logger _logger;
 
         protected Repository ( IDatabaseFactory databaseFactory )
         {
+            _logger = LogManager.GetCurrentClassLogger( );
             DatabaseFactory = databaseFactory;
-            _dbset = DataContext.Set<T> ( );
+            _dbset = DataContext.Set<T>( );
         }
 
         protected IDatabaseFactory DatabaseFactory { get; }
 
-        protected DatabaseContext DataContext => _dataContext ?? ( _dataContext = DatabaseFactory.Get ( ) );
+        protected DatabaseContext DataContext => _dataContext ?? ( _dataContext = DatabaseFactory.Get( ) );
 
         public virtual void Add ( T entity )
         {
@@ -31,57 +34,139 @@ namespace wallchat.DAL.App.Implementations
                 _dbset.Add (entity);
                 DataContext.SaveChanges( );
             }
-            catch (Exception ex)
+            catch ( Exception ex )
             {
-                //logger
-                throw new DalException ("DAL exception, error with Adding " + typeof ( T ).FullName + " entity");
+                _logger.Error ("Repository exception, error with ADD " + typeof ( T ).FullName + " entity");
+                _logger.Error ("EXCEPTION: " + ex);
+                throw new RepositoryException (
+                    "Repository exception, error with ADD " + typeof ( T ).FullName + " entity");
             }
         }
 
         public virtual void Update ( T entity )
         {
-            _dbset.Attach (entity);
-            _dataContext.Entry (entity).State = EntityState.Modified;
-            DataContext.SaveChanges();
+            try
+            {
+                _dbset.Attach(entity);
+                _dataContext.Entry(entity).State = EntityState.Modified;
+                DataContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Repository exception, error with UPDATE " + typeof(T).FullName + " entity");
+                _logger.Error("EXCEPTION: " + ex);
+                throw new RepositoryException(
+                    "Repository exception, error with UPDATE " + typeof(T).FullName + " entity");
+            }
         }
 
         public virtual void Delete ( T entity )
         {
-            _dbset.Attach (entity);
-            _dataContext.Entry (entity).State = EntityState.Deleted;
-            DataContext.SaveChanges ( );
+            try
+            {
+                _dbset.Attach(entity);
+                _dataContext.Entry(entity).State = EntityState.Deleted;
+                DataContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Repository exception, error with DELETE " + typeof(T).FullName + " entity");
+                _logger.Error("EXCEPTION: " + ex);
+                throw new RepositoryException(
+                    "Repository exception, error with DELETE " + typeof(T).FullName + " entity");
+            }
         }
 
         public virtual void Delete ( Expression<Func<T, bool>> where )
         {
-            var objects = _dbset.Where (where).AsEnumerable ( );
-            foreach ( var obj in objects )
-                _dbset.Remove (obj);
+            try
+            {
+                var objects = _dbset.Where(where).AsEnumerable();
+                foreach (var obj in objects)
+                    _dbset.Remove(obj);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Repository exception, error with DELETE " + typeof(T).FullName + " entity");
+                _logger.Error("EXCEPTION: " + ex);
+                throw new RepositoryException(
+                    "Repository exception, error with DELETE " + typeof(T).FullName + " entity");
+            }
         }
 
         public virtual T GetById ( long id )
         {
-            return _dbset.Find (id);
+            try
+            {
+                return _dbset.Find(id);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Repository exception, error with GET BY ID " + typeof(T).FullName + " entity");
+                _logger.Error("EXCEPTION: " + ex);
+                throw new RepositoryException(
+                    "Repository exception, error with GET BY ID " + typeof(T).FullName + " entity");
+            }
         }
 
         public virtual T GetById ( string id )
         {
-            return _dbset.Find (id);
+            try
+            {
+                return _dbset.Find(id);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Repository exception, error with GET BY ID " + typeof(T).FullName + " entity");
+                _logger.Error("EXCEPTION: " + ex);
+                throw new RepositoryException(
+                    "Repository exception, error with GET BY ID " + typeof(T).FullName + " entity");
+            }
         }
 
-        public virtual IEnumerable<T> GetAll ()
+        public virtual IEnumerable<T> GetAll()
         {
-            return _dbset.ToList ( );
+            try
+            {
+                return _dbset.ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Repository exception, error with GET ALL " + typeof(T).FullName + " entity");
+                _logger.Error("EXCEPTION: " + ex);
+                throw new RepositoryException(
+                    "Repository exception, error with GET ALL " + typeof(T).FullName + " entity");
+            }
         }
 
         public virtual IEnumerable<T> GetMany ( Expression<Func<T, bool>> where )
         {
-            return _dbset.Where (where).ToList ( );
+            try
+            {
+                return _dbset.Where(where).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Repository exception, error with GET MANY " + typeof(T).FullName + " entity");
+                _logger.Error("EXCEPTION: " + ex);
+                throw new RepositoryException(
+                    "Repository exception, error with GET MANY " + typeof(T).FullName + " entity");
+            }
         }
 
         public T Get ( Expression<Func<T, bool>> where )
         {
-            return _dbset.Where (where).FirstOrDefault ( );
+            try
+            {
+                return _dbset.Where(where).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Repository exception, error with GET BY EXPRESSION " + typeof(T).FullName + " entity");
+                _logger.Error("EXCEPTION: " + ex);
+                throw new RepositoryException(
+                    "Repository exception, error with GET BY EXPRESSION " + typeof(T).FullName + " entity");
+            }
         }
     }
 }
