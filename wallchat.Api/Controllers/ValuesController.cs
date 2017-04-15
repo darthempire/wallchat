e38d7e843c88d865b00a4a66939c349eb7f856fd
alexandr <alexandrlukashov1997@gmail.com>
2017-04-15 17:28:14 +0300

    hello

--- wallchat.Api/Controllers/ValuesController.cs
+++ wallchat.Api/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace wallchat.Api.Controllers
         {
             _userService = userService;
         }
-
+        //hello
         // GET api/<controller>
         public IEnumerable<string> Get()
         {
