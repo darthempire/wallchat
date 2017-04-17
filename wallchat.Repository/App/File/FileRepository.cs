ed87286eedce9e2a6710e49e6f44aeb1567b6c47
GhostBY <alexandrlukashov@outlook.com>
2017-04-17 17:09:55 +0300

    add DTO model for File and create service

---
+++ wallchat.Repository/App/File/FileRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wallchat.DAL.App.Contracts;
+using wallchat.DAL.App.Implementations;
+using wallchat.Model.App.EF;
+
+namespace wallchat.Repository.App.File
+{
+    public class FileRepository:Repository<Model.App.Entity.File>,IFileRepository
+    {
+        private DatabaseContext _dataContext;
+
+        public FileRepository(IDatabaseFactory databaseFactory)
+            : base (databaseFactory)
+        {
+            DatabaseFactory = databaseFactory;
+        }
+        protected IDatabaseFactory DatabaseFactory { get; }
+
+        protected DatabaseContext DataContext
+        {
+            get { return _dataContext ?? (_dataContext = DatabaseFactory.Get()); }
+        }
+    }
+    public interface IFileRepository : IRepository<Model.App.Entity.File>
+    {
+    }
+}
---
+++ wallchat.Service/Contracts/IFileService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wallchat.Model.App.DTO;
+
+namespace wallchat.Service.Contracts
+{
+    public interface IFileService
+    {
+        FileDTO Find ( long id );
+        void Remove ( long id );
+        void Update ( FileDTO fileDto );
+        List<FileDTO> GetAll ();
+        void Create ( RegisterFileDTO registerFileDto );
+    }
+}
